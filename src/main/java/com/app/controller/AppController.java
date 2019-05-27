6383c99818baa9b617f0fcff783c2872147f9a72
Daniel <daniellegawiec20@gmail.com>
2019-05-27 15:47:55 +0200

    Dodanie panelu rejestracji

--- src/main/java/com/app/controller/AppController.java
+++ src/main/java/com/app/controller/AppController.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @EnableAutoConfiguration
@@ -11,6 +12,16 @@ public class AppController {
 
     @RequestMapping(value={"/", "/home"})
     public String home(ModelMap model) {
+        return "index";
+    }
+
+    @RequestMapping(value="/login", method=RequestMethod.GET)
+    public String loginPage(){
         return "login";
+    }
+
+    @RequestMapping(value="/register", method=RequestMethod.GET)
+    public String registerPane(){
+        return "register";
     }
 }
